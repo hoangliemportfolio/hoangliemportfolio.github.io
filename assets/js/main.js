5afd7638ad3e6793fd273c1b977cf17520ddb3f0
hoangliem1997 <liemph.1997@gmail.com>
2021-07-01 18:11:27 +0700

    g

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,28 @@
 
 
 
-// Active cho nút chọn Portfolio
-var portSelection = document.querySelectorAll('.port__navbar-item')
+// // Active cho nút chọn Portfolio
+// var portSelection = document.querySelectorAll('.port__navbar-item')
 
-for (let i = 0; i < portSelection.length; i++) {
-    portSelection[i].addEventListener('click', function(){
-        for (let j = 0; j < portSelection.length; j++){
-            portSelection[j].classList.remove('active');
+// for (let i = 0; i < portSelection.length; i++) {
+//     portSelection[i].addEventListener('click', function(){
+//         for (let j = 0; j < portSelection.length; j++){
+//             portSelection[j].classList.remove('active');
+//         }
+//         this.classList.add('active');})}
+
+
+$(document).ready(function(){
+    var check = document.querySelector('.check')
+
+    check.addEventListener('click', function(){
+
+        if($('.port__navbar-list').hasClass('active1')) {
+            $('.port__navbar-list').removeClass('active1').addClass('active2')
         }
-        this.classList.add('active');})}
+
+    })
+
+
+
+})
